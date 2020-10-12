a6b9c99ff9944f270f05bb8f5b3dd93e8908b062
root <root@vmi359523.contaboserver.net>
2020-10-12 23:53:59 +0200

    what

--- backup.sh
+++ backup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-tar zcf world-`date +%Y%m%d`.tar.gz world
+tar zcf world-`date +%Y%m%d`.tar.gz ../world
+find world-* -type f -mtime +10 -delete
 
-find ../world-* -type f -mtime +10 -delete
-
+mkdir latest_backup
 var=$(find world-* -type f -mtime 0)
 /bin/cp -rf $var "latest_backup/world.tar.gz"
