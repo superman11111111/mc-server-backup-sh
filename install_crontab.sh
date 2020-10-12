4eb84dae19e6137bf0657df75ee7ac372b7e93f3
m-kypr <mrckyper@gmail.com>
2020-10-13 01:16:56 +0200

    w

--- install_crontab.sh
+++ install_crontab.sh
@@ -1,2 +1,4 @@
+/bin/cp "start.sh" ..
 echo "0 0 * * * cd ${PWD} && ./backup.sh >/dev/null 2>&1" > /etc/cron.d/backup_minecraft_world
+echo "@reboot cd ${PWD}/../ && ./start.sh >/dev/null 2>&1" > /etc/cron.d/start_minecraft_server
 
--- install_start_and_crontab.sh
+++
@@ -1,3 +0,0 @@
-/bin/cp "start.sh" ..
-echo "@reboot cd ${PWD}/../ && ./start.sh >/dev/null 2>&1" > /etc/cron.d/start_minecraft_server
-
